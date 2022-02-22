0f3094bb489dae1f53d3198b3fe20f3a8655382f
redmac135 <ethanzhao1981@gmail.com>
2022-02-22 10:38:30 -0500

    add date field to reflection entries

--- reflection/models.py
+++ reflection/models.py
@@ -8,12 +8,16 @@ from .choices import ADJECTIVE_CHOICES, REASON_CHOICES
 
 class ReflectionEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateField(auto_now_add=True)
     feeling = models.IntegerField()
     adjective = MultiSelectField(choices=ADJECTIVE_CHOICES, max_choices=3)
     reason = MultiSelectField(choices=REASON_CHOICES, max_choices=3)
     rose = models.CharField(max_length=500, blank=True)
     bud = models.CharField(max_length=500, blank=True)
     thorn = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.user + " " + str(self.date)
 
 class AdjectiveChoice(models.Model):
     feeling = models.IntegerField()
