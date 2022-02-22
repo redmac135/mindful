d0ddc1a7be2144c73625f67ffc16079fd724be3c
redmac135 <ethanzhao1981@gmail.com>
2022-02-22 10:42:20 -0500

    Fix user signup error

--- users/views.py
+++ users/views.py
@@ -6,22 +6,24 @@ from .models import Profile
 
 # Create your views here.
 def update_user_data(user):
-    Profile.objects.update_or_create(user=user, defaults={'email': user.profile.email, 'token': user.profile.token})
+    Profile.objects.update_or_create(user=user, defaults={'email': user.email, 'token': user.token})
 
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save() # Create User in Default User Model
             user.refresh_from_db()
 
-            user.profile.email = form.cleaned_data.get('email')
-            user.profile.token = form.cleaned_data.get('token')
+            # Add Profile Attributes to User Object
+            user.email = form.cleaned_data.get('email')
+            user.token = form.cleaned_data.get('token')
+
+            # Create Profile Object
             update_user_data(user)
 
-            user.save()
+            # Login to Newly Created User
             raw_password = form.cleaned_data.get('password1')
- 
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
 
