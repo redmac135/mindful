f8c53a1a7c0e3e089518313dbc2639154c6eaf7a
redmac135 <ethanzhao1981@gmail.com>
2022-02-22 11:08:07 -0500

    Add Signin Form
    Added through auth.urls defaults, overridden in users/templates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup_view, name="reflection"),
+    path('signup/', views.signup_view, name="signup"),
+    path('', include('django.contrib.auth.urls'))
 ]
