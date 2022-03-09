b6a940ab44686a298820fe180744ad1e0a46b7d4
redmac135 <ethanzhao1981@gmail.com>
2022-03-09 12:10:39 -0500

    revmoe formtools wizard app
    Uninstall formtools in setting and pip
    Create mulistep form from scratch
    Delete form.html for Wizard

--- reflection/views.py
+++ reflection/views.py
@@ -1,44 +1,47 @@
 from django.shortcuts import render, redirect
-from .models import ReflectionEntry, AdjectiveChoice
+from django.contrib.auth.decorators import login_required
+from .models import ReflectionEntry
 from .forms import FormReflectionOne, FormReflectionTwo
-from formtools.wizard.views import SessionWizardView
-from django.contrib import messages
-from datetime import date
-
-from .choices import ADJECTIVE_CHOICES_1, ADJECTIVE_CHOICES_2, ADJECTIVE_CHOICES_3, ADJECTIVE_CHOICES_4, ADJECTIVE_CHOICES_5
 
 # Create your views here.
 
-class FormWizardView(SessionWizardView):
-    template_name = "reflection/form.html"
-    form_list = [FormReflectionOne, FormReflectionTwo, ]
+def reflection_formview(request):
+    if request.method == 'POST':
+        if request.POST.get('ReflectionForm2'):
+            form1 = FormReflectionOne(request.POST)
+            if form1.is_valid():
+                request.session['Data_ReflectionForm1'] = form1.cleaned_data.get('feeling')
+                if 'Data_ReflectionForm2' in request.session:
+                    x = request.session['Data_ReflectionForm2']
+                else:
+                    x = None
+                form2 = FormReflectionTwo(initial = x, feeling=request.session['Data_ReflectionForm1'])
+                return render(request, 'reflection/form2.html', {'form2': form2})
+            else:
+                return render(request, 'reflection/form1.html', {'form1': form1})
+        elif request.POST.get('ReflectionForm1'):
+            form1 = FormReflectionOne()
+            return render(request, 'reflection/form1.html', {'form1': form1})
+        elif request.POST.get('submit'):
+            form2 = FormReflectionTwo(feeling = request.session['Data_ReflectionForm1'], data=request.POST)
+            if form2.is_valid() and request.user.is_authenticated:
+                data_form2 = form2.cleaned_data
+                feeling = request.session['Data_ReflectionForm1']
+                adjective = data_form2.get('adjective')
+                reason = data_form2.get('reason')
+                ReflectionEntry.objects.create(
+                    user=request.user,
+                    feeling=feeling,
+                    adjective=adjective,
+                    reason=reason,
+                )
+            return redirect('dashboard')
+                
+    else:
+        form1 = FormReflectionOne()
+        return render(request, 'reflection/form1.html', {'form1': form1})
 
-    def done(self, form_list, **kwargs):
-        form_data = [form.cleaned_data for form in form_list]
-        if self.request.user:
-            if ReflectionEntry.objects.filter(user=self.request.user, date=date.today()).exists():
-                ReflectionEntry.objects.filter(user=self.request.user).delete()
-            ReflectionEntry.objects.create(**form_data[1], feeling=form_data[0]['feeling'], user=self.request.user)
-            messages.success(self.request, 'Your reflection entry was saved.') # Add Success Message onto Templates
-        return redirect('home')
-    
-    # attempt at making dynamic choices based on first choice
-    def get_form(self, step=None, data=None, files=None):
-        if step is None:
-            step = self.steps.current
-
-        if step == '1':
-            firststep_data = self.get_cleaned_data_for_step('0')
-            choices_list = AdjectiveChoice.objects.filter(feeling=firststep_data['feeling'])
-            choices = [(x.order, x.adjective) for x in choices_list]
-            form = FormReflectionTwo(choices)
-        else:
-            return super(FormWizardView, self).get_form(step, data, files)
-        
-        return form
-
-    def get_context_data(self, form, **kwargs):
-        context = super(FormWizardView, self).get_context_data(form=form, **kwargs)
-        if ReflectionEntry.objects.filter(user=self.request.user, date=date.today()).exists():
-            context.update({'completed': True})
-        return context
+@login_required
+def dashboard_view(request):
+    reflections = ReflectionEntry.objects.filter(user=request.user)
+    return render(request, 'reflection/dashboard.html', {'entries': reflections})
