d6cb4a6cdf5b955f4e0e023a333bc23b28958fa3
redmac135 <ethanzhao1981@gmail.com>
2022-03-09 12:30:31 -0500

    add limitChoice.js to form2
    Change limit to 3 or less

--- reflection/static/reflection/js/limitChoice.js
+++ reflection/static/reflection/js/limitChoice.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#id_reason :input").change(function(event) {
-    if ($("#id_reason :input:checked").length > 2) {this.checked = false;}
+    if ($("#id_reason :input:checked").length > 3) {this.checked = false;}
     });
     $("#id_adjective :input").change(function(event) {
-      if ($("#id_adjective :input:checked").length > 2) {this.checked = false;}
+      if ($("#id_adjective :input:checked").length > 3) {this.checked = false;}
     });
   });
