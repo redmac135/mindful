85c159ae87a912e2037391724faac02717d35076
bryan <bryan.deng002@gmail.com>
2022-03-24 18:05:29 -0400

    finalized postcss installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
     content: [
-        "./assets/**/*.js", 
-        './templates/**/*.html'
+        "./assets/**/*.js",
+        './templates/**/*.html',
+        './reflection/templates/reflection/*.html',
     ],
     theme: {
         extend: {},
