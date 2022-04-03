642b0c9c06f77ae64e033090d1aab2f7a24a98d5
bryan <bryan.deng002@gmail.com>
2022-04-02 22:09:05 -0400

    update navbar links for about us and faq pages

--- about/forms.py
+++ about/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Question
 
+# TODO: get email directly from logged in user
+# TODO: only allow question to be submitted if user is logged in
 class QuestionForm(forms.Form):
     email = forms.EmailField()
     question = forms.CharField(label="Question", max_length=300)
--- about/urls.py
+++ about/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import AboutUsView, FAQView
 
 urlpatterns = [
-    path("", AboutUsView.as_view(), name="about_us"),
+    path("", AboutUsView.as_view(), name="about"),
     path("faq/", FAQView.as_view(), name="faq"),
 ]
