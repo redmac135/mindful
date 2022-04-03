3f400dbac94825dbd54a7841f250970cb7290942
bryan <bryan.deng002@gmail.com>
2022-04-03 13:19:10 -0400

    remove redundant code in utils

--- reflection/utils.py
+++ reflection/utils.py
@@ -17,13 +17,8 @@ class ReflectionCalendar(HTMLCalendar):
             for event in reflections_per_day
         )
 
-        if day == today.day:
-            istoday = 'istoday'
-        else:
-            istoday = ''
-
         if day != 0:
-            return f"<td class='{istoday}'><div class='date'>{day}</div><ul class='feeling-icon'> {d} </ul></td>"
+            return f"<td{' class=\"idtoday\"' if day == today.day else ''}><div class='date'>{day}</div><ul class='feeling-icon'> {d} </ul></td>"
         return "<td></td>"
 
     def formatweek(self, theweek, events, today):
