1cb9a57fdaae0e2a7b00f49944e53c82b92dac2d
bryan <bryan.deng002@gmail.com>
2022-04-02 22:49:39 -0400

    style password reset pages

--- users/templatetags/fieldfilter.py
+++ users/templatetags/fieldfilter.py
@@ -2,11 +2,13 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='addclass')
-def addclass(value, arg):
-    return value.as_widget(attrs={'class': arg})
 
-@register.filter(name='placeholder_classes')
+@register.filter(name="addclass")
+def addclass(value, arg):
+    return value.as_widget(attrs={"class": arg})
+
+
+@register.filter(name="placeholder_classes")
 def placeholder_class(value, args):
-    placeholder, classes = args.split(',', 1)
-    return value.as_widget(attrs={'placeholder': placeholder, 'class': classes})
+    placeholder, classes = args.split(",", 1)
+    return value.as_widget(attrs={"placeholder": placeholder, "class": classes})
