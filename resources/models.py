93dbaa9557641fc1437fa9613794c546c0a733e7
bryan <bryan.deng002@gmail.com>
2022-04-07 22:57:02 -0400

    add resources page

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    link = models.URLField(max_length=300)
+    resource_t = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- resources/views.py
+++ resources/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views.generic import View
+from .models import Resource
 
 # Create your views here.
 
 
-class ResourcesView(TemplateView):
-    template_name = "resources/resources.html"
+class ResourcesView(View):
+    template_name = "resources/resources_form.html"
+
+    def get(self, request):
+        t = request.GET.get("t", "")
+        if t == "all":
+            print(Resource.objects.all())
+            return render(
+                request,
+                "resources/resources_list.html",
+                {"resources": Resource.objects.all()},
+            )
+        if t in {"hotline", "counselling", "cbt"}:
+            return render(
+                request,
+                "resources/resources_list.html",
+                {"resources": Resource.objects.filter(resource_t=t)},
+            )
+        return render(request, self.template_name)
