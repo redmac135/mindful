bbfbae87419f8d3b5f034ad7ebcbd73174370ffe
redmac135 <ethanzhao1981@gmail.com>
2022-04-12 20:26:56 -0400

    Fix broken character limit

--- mindful/resources/models.py
+++ mindful/resources/models.py
@@ -7,7 +7,7 @@ class Resource(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     link = models.URLField(max_length=300)
-    resource_t = models.CharField(max_length=10)
+    resource_t = models.CharField(max_length=15)
 
     def __str__(self):
         return self.name
