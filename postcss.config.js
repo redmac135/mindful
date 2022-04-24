311c63c83cdbc56249f4dafe71a667fd5d80da0b
bryan <bryan.deng002@gmail.com>
2022-04-24 12:48:28 -0400

    remove postcss bug

--- postcss.config.js
+++ postcss.config.js
@@ -8,16 +8,5 @@ module.exports = {
         cssnano({
             preset: "default",
         }),
-        purgecss({
-            content: [
-                "./mindful/assets/**/js/*.js",
-                "./mindful/templates/**/*.html",
-                "./mindful/**/templates/**/*.html",
-                "./mindful/**/static/**/js/*.js",
-                "./mindful/node_modules/flowbite/**/*.js",
-            ],
-            defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
     ],
 };
