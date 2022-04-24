136cfec0e3c5b0645e5904da0fba4f1e112eefa3
bryan <bryan.deng002@gmail.com>
2022-04-24 11:40:28 -0400

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,23 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
     plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
+        require("tailwindcss"),
+        require("autoprefixer"),
+        cssnano({
+            preset: "default",
+        }),
+        purgecss({
+            content: [
+                "./mindful/assets/**/js/*.js",
+                "./mindful/templates/**/*.html",
+                "./mindful/**/templates/**/*.html",
+                "./mindful/**/static/**/js/*.js",
+                "./mindful/node_modules/flowbite/**/*.js",
+            ],
+            defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
     ],
 };
