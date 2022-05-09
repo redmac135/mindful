ba4c09a8afbab4abd5f072bde9e85c85d0daa076
redmac135 <ethanzhao1981@gmail.com>
2022-05-09 18:50:10 -0400

    breathing app not working version bundle fails

--- mindful/assets/miniapps/js/breathing.js
+++ mindful/assets/miniapps/js/breathing.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Animated } from 'react-native';
+
+const c = new Animated.Value(0);
 
 class App extends Component {
     render() {
+        React.useEffect(() => {
+            Animated.loop(
+                Animated.timing(c, {
+                    toValue: 300,
+                    duration: 3000,
+                }),
+                { iterations: 5 }
+            ).start();
+        }, []);
+
+        const animatedColor = c.interpolate({
+            inputRange: [0, 200, 300],
+            outputRange: ['orange', 'lightgreen', 'yellow'],
+        });
+
         return (
-            <h1>Hi There!</h1>
-        )
+            <Animated.View style={{ backgroundColor: animatedColor }}>
+                <h1>Hi There!</h1>
+            </Animated.View>
+        );
     }
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: {
     navbar: './mindful/assets/base/js/navbar.js',
     breathing: './mindful/assets/miniapps/js/breathing.js',
@@ -13,10 +14,14 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+        options: {
+          presets: [
+            "@babel/preset-env", 
+            ["@babel/preset-react", { 'runtime': 'automatic' }],
+          ] 
+        }
       },
-    ]
+    ],
   },
 };
